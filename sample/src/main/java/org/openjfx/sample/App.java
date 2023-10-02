fab051840cf71837aac3dfe799192a6b5e719754
Kastner-Masilko.Chri <kastner-masilko.christoph@htlwrn.ac.at>
2023-10-02 10:18:45 +0200

    Arango AQL in Java

--- sample/src/main/java/org/openjfx/sample/App.java
+++ sample/src/main/java/org/openjfx/sample/App.java
@@ -4,7 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import java.util.*;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import com.arangodb.*;
+import com.arangodb.entity.BaseDocument;
+import com.arangodb.util.RawJson;
 
 import java.io.IOException;
 
@@ -14,10 +19,21 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    public static final ArangoDB arangodb = new ArangoDB.Builder().host("localhost", 8529).password("").build();
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
+        try
+        {
+            String query = "FOR t IN commits RETURN t.signature";
+            ArangoCursor cursor = arangodb.db("binocular-Binocular").query(query, null, null, BaseDocument.class);
+            cursor.forEachRemaining(aDocument -> {System.out.println("Key: " + aDocument.getKey());});
+        }
+        catch(Exception e)
+        {
+
+        }
+        scene = new Scene(new Label("Label"), 200, 200);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
@@ -36,4 +52,11 @@ public class App extends Application {
         launch();
     }
 
+    /*
+    private static void cleanup()
+    {
+        ArangoDatabase db = App.arangoDB.db("mydb");
+        if (db.exists()) db.drop();
+    }
+    */
 }
