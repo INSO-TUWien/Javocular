561679b7691782575a39b788b00c88366908a326
Kastner-Masilko.Chri <kastner-masilko.christoph@htlwrn.ac.at>
2023-10-16 08:26:19 +0200

    :)

---
+++ sample/jfreechart/src/main/java/javocular/Main.java
@@ -0,0 +1,13 @@
+package javocular;
+
+import java.awt.*;
+
+public class Main extends Frame{
+    public static void main(String[] args) {
+        Main mf = new Main();
+        mf.setSize(500, 300);
+        mf.setTitle("Danijel ist ein HuSo");
+        mf.setVisible(true);
+        mf.setBackground(Color.BLUE);
+    }
+}
