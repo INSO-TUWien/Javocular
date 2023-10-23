83b77263ffd496afb1b625e0c705f1b7ad5084aa
Kastner-Masilko.Chri <kastner-masilko.christoph@htlwrn.ac.at>
2023-10-23 08:07:25 +0200

    Java ArangoDB read code

---
+++ UI/src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.List;
+
+import com.arangodb.entity.BaseDocument;
+import com.arangodb.*;
+import com.arangodb.util.RawJson;
+
+public class Main{
+    private static JFrame frame;
+    private static ArangoDB arangodb;
+
+    public static void main(String[] args) {
+        arangodb = new ArangoDB.Builder().host("localhost", 8529).user("root").password("").build();
+            EventQueue.invokeLater(() -> {
+            try {
+                frame = new JFrame();
+                JPanel pnl = new JPanel();
+                pnl.setBackground(Color.WHITE);
+                frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                frame.setSize(400, 300);
+                frame.setContentPane(pnl);
+                frame.setLayout(new FlowLayout());
+                frame.setVisible(true);
+
+                String query = "FOR t IN commits RETURN t";
+                ArangoCursor<BaseDocument> cursor = arangodb.db("binocular-Binocular").query(query, BaseDocument.class, null, null);
+                while(cursor.hasNext()) pnl.add(new JLabel(cursor.next().getAttribute("signature").toString()));
+            } catch (Exception e) {
+                System.exit(1);
+            }
+        });
+    }
+}
