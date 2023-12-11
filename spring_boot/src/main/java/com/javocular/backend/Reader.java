000771b664781068b6f3de08bdaaeecc47e5604a
Kastner-Masilko.Chri <kastner-masilko.christoph@htlwrn.ac.at>
2023-12-11 09:48:50 +0100

    Spring-Boot Application + Simple REST test call

---
+++ spring_boot/src/main/java/com/javocular/backend/Reader.java
@@ -0,0 +1,37 @@
+package com.javocular.backend;
+
+import com.arangodb.entity.BaseDocument;
+import com.arangodb.*;
+import com.arangodb.util.RawJson;
+import com.fasterxml.jackson.databind.ser.Serializers;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Reader{
+    private ArangoDB arangodb;
+    private String connector;
+
+    public Reader(){
+        arangodb = null;
+        connector = "";
+    }
+
+    public void ChangeType(String connection) {
+        arangodb = new ArangoDB.Builder().host("localhost", 8529).user("root").password("").build();
+        connector = connection;
+    }
+
+    public List<String> QueryResult(String query) {
+
+        if(arangodb == null) return null;
+        List<String> result = new ArrayList<>();
+        ArangoCursor<BaseDocument> cursor = arangodb.db(connector).query(query, BaseDocument.class, null, null);
+        while(cursor.hasNext()) result.add(cursor.next().toString());
+        return result;
+    }
+
+    public void reset(String connection)
+    {
+        this.ChangeType(connection);
+    }
+}
--- spring_boot/src/main/java/com/javocular/backend/TestController.java
+++ spring_boot/src/main/java/com/javocular/backend/TestController.java
@@ -2,12 +2,17 @@ package com.javocular.backend;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class TestController {
-    @RequestMapping("/")
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public @ResponseBody String greeting() {
-        return "Hello World";
+        Reader reader = new Reader();
+        reader.reset("binocular-Binocular");
+        return reader.QueryResult("FOR t IN commits RETURN t").get(1);
     }
 
 }
