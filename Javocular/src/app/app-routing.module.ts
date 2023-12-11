3e225087a09ad1b02a6e87b2b50c162efbb799da
ManuelMacheiner <Macheiner.Manuel@student.htlwrn.ac.at>
2023-12-11 09:50:07 +0100

    created home page

--- Javocular/src/app/app-routing.module.ts
+++ Javocular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent} from "./home/home.component";
+import {PlotlyComponentComponent} from "./plotly-component/plotly-component.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'graphs', component: PlotlyComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Javocular/src/app/app.module.ts
+++ Javocular/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PlotlyComponentComponent } from './plotly-component/plotly-component.component';
+import { PlotlyComponentComponent} from "./plotly-component/plotly-component.component";
+import {HomeComponent} from "./home/home.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlotlyComponentComponent
+    PlotlyComponentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
