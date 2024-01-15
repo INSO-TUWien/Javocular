25cd71f2db8706348b6ef31d935cdf3c9f062251
Danijel2701 <danijelj2701@gmail.com>
2024-01-15 08:57:29 +0100

    added buttons and layout for histogram

--- Javocular/src/app/app.module.ts
+++ Javocular/src/app/app.module.ts
@@ -8,11 +8,13 @@ import {HomeComponent} from "./home/home.component";
 import { HttpClientModule } from '@angular/common/http';
 import {NgOptimizedImage} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import {HistogramCIMRComponent} from "./histogram-cimr/histogram-cimr.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     BarcharCIMRComponent,
+    HistogramCIMRComponent,
     HomeComponent
   ],
   imports: [
